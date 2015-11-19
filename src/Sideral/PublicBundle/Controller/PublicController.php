860f27c512d5cbda61d87ddb665266cd49a353ae
Alexis Lebrun <Alexis Lebrun>
2015-11-19 11:28:41 +0100

    Ajout de la page Ressources et triple héritage de template

--- src/Sideral/PublicBundle/Controller/PublicController.php
+++ src/Sideral/PublicBundle/Controller/PublicController.php
@@ -9,11 +9,20 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class PublicController extends Controller
 {
     /**
-     * @Route("/", name="sideral_public_accueil")
+     * @Route("/", name="_sideral_public_accueil")
      * @Template()
      */
     public function indexAction()
     {
         return array();
     }
+    /**
+     * @Route("/ressources", name="_sideral_public_ressources")
+     * @Template()
+     */
+    public function resourcesAction()
+    {
+        return array();
+    }
+
 }
